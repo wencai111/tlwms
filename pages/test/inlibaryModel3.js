b632a66d7a3be22745fa5f816bc75fa7cb771851
1259310290@qq.com <1259310290@qq.com>
2019-03-27 23:05:12 +0800

    练习3

---
+++ pages/test/inlibaryModel3.js
@@ -0,0 +1,31 @@
+function inlibraryModel(option) {
+	this.code = "";//物料名称
+	this.TotalAmount = 0;//物料总数
+	this.goods = [];//物料个数
+	this.codeid = "";//物料ID
+	this.storageName = "";//物料货架
+	//首次新增物料
+	this.setMaterial = function(option) {
+		this.code = option.code;//物料名称赋值
+		this.TotalAmount = option.TotalAmount;//物料总数
+		this.goods.push()//物料个数
+		this.codeid =  option.codeid;//物料ID
+	};
+	//物料相加
+	this.addGoods = function(option) {
+		var storage =new inlibraryModel()
+        this.goods.push();//物料个数
+		this.TotalAmount = this.TotalAmount + this.TotalAmount; //总数相加
+	}
+	this.setInlibrary=function(option){
+		debugger;
+		this.storageName=option.storageName;	
+	}
+	    if(option){
+		this.init(option);
+	}
+}
+
+	
+const inlibraryModels = new inlibraryModel(option);
+export default inlibraryModels;
