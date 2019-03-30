740f95ea7c9e0496cf13eee3f225d1c2ca2eedf5
wencai111 <1259310290@qq.com>
2019-03-30 10:45:38 +0800

    修改pages

--- pages/main/main.vue
+++ pages/main/main.vue
@@ -4,11 +4,7 @@
 			<uni-grid :data="menu" v-on:click="goMainPage"></uni-grid>
 			<view style="height: 40upx;"></view>
 			<uni-list>
-				<uni-list-item
-					title="退出登录"
-					show-extra-icon="true"
-					:extra-icon="{ color: '#4cd964', size: '22', type: 'closeempty' }"
-				></uni-list-item>
+				<uni-list-item title="退出登录" show-extra-icon="true" :extra-icon="{ color: '#4cd964', size: '22', type: 'closeempty' }"></uni-list-item>
 			</uni-list>
 		</view>
 		<view v-if="!hasLogin" class="message-container">
@@ -22,119 +18,134 @@
 </template>
 
 <script>
-import { uniGrid, uniList, uniListItem } from '@dcloudio/uni-ui';
-import { mapState } from 'vuex';
-import { authAccount } from '@/libs/util.js';
-
-export default {
-	data() {
-		return {
-			menu: [
-				{
-					image: '/static/img/inLibrary.png',
-					text: '入库管理',
-					url: '../inlibrary/inway'
-				},
-				{
-					image: '/static/img/outLibrary.png',
-					text: '出库管理',
-					fdf: 'fdf',
-					url: '../outlibrary/inway'
-				},
-				{
-					image: '/static/img/deliveryNote.png',
-					text: '盘点',
-					url: '../deliveryNote/index'
-				},
-				{
-					image: '/static/img/service.png',
-					text: '维修',
-					url: '../service/index'
-				},
-				{
-					image: '/static/img/exchange.png',
-					text: '换货',
-					url: '../exchange/inway'
-				},
-				{
-					image: '/static/img/Return.png',
-					text: '不良品',
-					url: '../Return/inway'
-				},
-				{
-					image: '/static/img/checkQuality.png',
-					text: '质检',
-					url: '../checkQuality/index'
-				},
-				{
-					image: '/static/img/CheckInventory.png',
-					text: '备件',
-					url: '../CheckInventory/inway'
-				},
-				{
-					image: '/static/img/query.png',
-					text: '查询',
-					url: '../query/index'
-				}
-			]
-		};
-	},
-	components: {
+	import {
 		uniGrid,
 		uniList,
 		uniListItem
-	},
-	computed: mapState(['forcedLogin', 'hasLogin', 'userName']),
-	methods: {
-		goMainPage: function(e) {
-			console.log(JSON.stringify(e));
-			uni.navigateTo({
-				url: this.$data.menu[e.index].url,
-				// url:'../main/main',
-				success:(e)=>{
-					console.log("fdfd:"+JSON.stringify(e))
-				},
-				fail: (e) => {
-				console.log("fd2fd:"+JSON.stringify(e))
-				}
-			});
-			console.log("end")
+	} from '@dcloudio/uni-ui';
+	import {
+		mapState
+	} from 'vuex';
+	import {
+		authAccount
+	} from '@/libs/util.js';
+
+	export default {
+		data() {
+			return {
+				menu: [{
+						image: '/static/img/inLibrary.png',
+						text: '入库管理',
+						url: '../inlibrary/inway'
+					},
+					{
+						image: '/static/img/outLibrary.png',
+						text: '出库管理',
+						fdf: 'fdf',
+						url: '../outlibrary/inway'
+					},
+
+					{
+						image: '/static/img/transfer.png',
+						text: '维修',
+						url: '../service/index'
+					},
+
+					{
+						image: '/static/img/Return.png',
+						text: '换货',
+						url: '../exchange/inway'
+					},
+
+					{
+						image: '/static/img/checkQuality.png',
+						text: '质检',
+						url: '../checkQuality/index'
+					},
+					{
+						image: '/static/img/transfer.png',
+						text: '拆装',
+						url: '../dismounting/inway'
+					},
+					{
+						image: '/static/img/checkInventory.png',
+						text: '盘点',
+						url: '../deliveryNote/index'
+					},
+					{
+						image: '/static/img/query.png',
+						text: '查询',
+						url: '../query/index'
+					},
+					{
+						image: '/static/img/Return.png',
+						text: '不良品',
+						url: '../Return/inway'
+					},
+					{
+						image: '/static/img/deliveryNote.png',
+						text: '备件',
+						url: '../CheckInventory/inway'
+					}
+				]
+			};
+		},
+		components: {
+			uniGrid,
+			uniList,
+			uniListItem
+		},
+		computed: mapState(['forcedLogin', 'hasLogin', 'userName']),
+		methods: {
+			goMainPage: function(e) {
+				console.log(JSON.stringify(e));
+				uni.navigateTo({
+					url: this.$data.menu[e.index].url,
+					// url:'../main/main',
+					success: (e) => {
+						console.log("fdfd:" + JSON.stringify(e))
+					},
+					fail: (e) => {
+						console.log("fd2fd:" + JSON.stringify(e))
+					}
+				});
+				console.log("end")
+			}
+		},
+		onLoad() {
+			console.log('登录状态：' + this.hasLogin);
+			authAccount(this.hasLogin, this.forcedLogin, this.userName);
 		}
-	},
-	onLoad() {
-		console.log('登录状态：' + this.hasLogin);
-		authAccount(this.hasLogin,this.forcedLogin,this.userName);
-	}
-};
+	};
 </script>
 
 <style>
-.message-container {
-	display: flex;
-	flex: 1;
-	flex-direction: column;
-}
+	.message-container {
+		display: flex;
+		flex: 1;
+		flex-direction: column;
+	}
 
-.title {
-	color: #8f8f94;
-	margin-top: 50upx;
-	text-align: center;
-}
+	.title {
+		color: #8f8f94;
+		margin-top: 50upx;
+		text-align: center;
+	}
 
-.ul {
-	font-size: 30upx;
-	color: #8f8f94;
-	margin-top: 50upx;
-}
+	.ul {
+		font-size: 30upx;
+		color: #8f8f94;
+		margin-top: 50upx;
+	}
 
-.ul > view {
-	line-height: 50upx;
-	text-align: center;
-}
+	.ul>view {
+		line-height: 50upx;
+		text-align: center;
+	}
 
-.logout {
-	color: red;
-	text-align: center;
-	border: 1upx solid #8f8f94;
-}
+	.logout {
+		color: red;
+		text-align: center;
+		border: 1upx solid #8f8f94;
+	}
 </style>
