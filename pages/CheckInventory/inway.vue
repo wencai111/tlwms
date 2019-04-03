09ab477f407f345520774f53c30c1aff966a310d
autoyu <yucihai@live.com>
2019-04-03 14:54:37 +0800

    修改pages.json

--- pages/CheckInventory/inway.vue
+++ pages/CheckInventory/inway.vue
@@ -2,10 +2,11 @@
 	<view class="content">
 		<view class="example-title">选择备件方式</view>
 		<uni-list>
-			<uni-list-item title="备件下架" note="" v-on:click="startMaterialsCheckInventory"></uni-list-item>
-			<uni-list-item title="备件打包" note="" v-on:click="startPackageCheckInventory"></uni-list-item>
-			<uni-list-item title="备件发货" note="" v-on:click="startMaterialsCheckInventory"></uni-list-item>
-			<uni-list-item title="备件物流查询" note="" v-on:click="startPackageCheckInventory"></uni-list-item>
+			<uni-list-item title="备件下架" note="" v-on:click="goOutlibrary"></uni-list-item>
+			<uni-list-item title="备件打包" note="" v-on:click="goPackeged"></uni-list-item>
+			<uni-list-item title="备件物流扫码" note="" v-on:click="goLogisticsScan"></uni-list-item>
+			<uni-list-item title="备件发货" note="" v-on:click="goShip"></uni-list-item>
+			<uni-list-item title="备件物流查询" note="" v-on:click="goLogisticsQuery"></uni-list-item>
 		</uni-list>
 	</view>
 </template>
@@ -21,13 +22,27 @@ export default {
 	components: { uniList, uniListItem },
 	computed: mapState(['forcedLogin', 'hasLogin', 'userName','code']),
 	methods: {
-		startMaterialsExchange: function() {
-			// uni.navigateTo({url:'/pages/exchange/index'});
+		//备件下架
+		goOutlibrary:function(){
+			uni.navigateTo({url:'/pages/checkInventory/outlibrary'});
 		},
-		startPackageExchange: function() {
-			// uni.redirectTo({ url: '/pages/exchange/index' });
-		//	uni.navigateTo({ url: '/pages/inlibrary/index' });
+		//备件打包
+		goPackeged:function(){
+			uni.navigateTo({url:'/pages/checkInventory/packeged'});
+		},
+		//备件物流扫码
+		goLogisticsScan:function(){
+			uni.navigateTo({url:'/pages/checkInventory/logisticsScan'});
+		},
+		//备件发货
+		goShip:function(){
+			uni.navigateTo({url:'/pages/checkInventory/ship'});
+		},
+		//备件物流查询
+		goLogisticsQuery:function(){
+			uni.navigateTo({url:'/pages/checkInventory/logisticsQuery'});
 		}
+		
 	},
 	onLoad() {
 		debugger;
