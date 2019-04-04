224cc68c4a2e3c9dc5af5537461996d600598216
autoyu <yucihai@live.com>
2019-04-05 02:36:28 +0800

    拣货码出库接口调试完成

--- api/outlibrary.js
+++ api/outlibrary.js
@@ -20,35 +20,36 @@ export const saveEmergentOutInfo = (data) => {
 }
 
 
-
 /**
  * @description 获取拣货码信息
- * 用于根据拣货码扫描出库前获取的相关信息，并进行界面的内容显示。，
+ * 用于根据拣货码扫描出库前获取的相关信息，并进行界面的内容显示。
  */
 export const getPickGoodsCodeInfo = (BillNum) => {
+	console.log("getPickGoodsCodeInfo入参：" + JSON.stringify(BillNum))
 	return uni.request({
-		url: domian + 'common/wms_Server_Data.asp?action= GetPickGoodsCodeInfo',
-		type: "POST",
+		url: domian + 'common/wms_Server_Data.asp?action=GetPickGoodsCodeInfo',
+		type: "GET",
 		dataType: "JSON",
 		data: {
-			"BillNum": BillNum
-		}
+			"BillNum":BillNum
+		},
 	});
 }
 
 /**
  * @description 根据拣货码扫描出库
- * 客户没有给出相关完整的出库计划或拣货时，用于的出库。
- * 用户可先通过手机端查看所需要出库的物料所在的库位分布信息，然后进行的出库操作，
+ * 客户没有给出相关完整的出库计划或拣货时，
+ * 用于的出库。用户可先通过手机端查看所需要
+ * 出库的物料所在的库位分布信息，然后进行的出库操作。
  */
 export const sureStockOut = (BillNum) => {
-	debugger;
+	console.log("sureStockOut入参：" + JSON.stringify(BillNum))
 	return uni.request({
-		url: domian + 'common/wms_Server_Data.asp?action= SureStockOut',
-		type: "POST",
+		url: domian + 'common/wms_Server_Data.asp?action=SureStockOut',
+		type: "GET",
 		dataType: "JSON",
 		data: {
-			"BillNum": BillNum
-		}
+			"BillNum":BillNum
+		},
 	});
 }
