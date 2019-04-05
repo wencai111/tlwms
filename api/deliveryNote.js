2ba587d2b2328f81260dac644788e3a22ec70632
autoyu <yucihai@live.com>
2019-04-05 14:41:10 +0800

    盘点接口调试完成

--- api/deliveryNote.js
+++ api/deliveryNote.js
@@ -4,57 +4,52 @@ const {
 } = config
 
 /**
-扫描库位，获取库位中相关物料信息
-作用：用于盘点前扫描库位码，获取对应库位中的物料信息。
+ * @description 扫描库位，获取库位中相关物料信息
+ * 用于盘点前扫描库位码，获取对应库位中的物料信息。
  */
-export const GetLocalMateInfo = (LocalID) => {
+export const getLocalMateInfo = (LocalID) => {
+	console.log("getLocalMateInfo入参：LocalID:" + LocalID);
 	return uni.request({
 		url: domian + 'common/wms_Server_Data.asp?action=GetLocalMateInfo',
-		type: "POST",
+		type: "GET",
 		dataType: "JSON",
 		data: {
-			LocalID: 'LocalID',
-			LocalName: 'LocalName',
-			MNumber: 'MNumber',
-			MName: 'MName',
-			SupplyName: 'SupplyName',
-			InDate: 'InDate',
-			EndQuan: 'EndQuan'
-		},
+			LocalID: 'LocalID'
+		}
 	});
 }
 /**
-盘点
-作用： 手机扫描库位码后，调用11接口直接获取当前库位物料（但不显示数量），然后手工输入盘点数量
+ * * @description盘点
+ *作用： 手机扫描库位码后，调用getLocalMateInfo接口直接获取当前库位物料（但不显示数量），
+ * 然后手工输入盘点数量
  */
-export const SaveInventoryInfo = (MNumber,PdQuan,LocalID) => {
+export const saveInventoryInfo = (data) => {
+	console.log("saveInventoryInfo入参：" + JSON.stringify(data));
 	return uni.request({
 		url: domian + 'common/wms_Server_Data.asp?action=SaveInventoryInfo',
-			type: "POST",
+		type: "GET",
 		dataType: "JSON",
-		data: {
-			"MNumber": MNumber,
-			"PdQuan": PdQuan,
-			"LocalID":LocalID
-		},
+		data: data
 	});
 }
 
 /**
-获取待盘点库位信息
-作用： 获取待盘点库位信息
+@description获取待盘点库位信息
+*作用： 获取待盘点库位信息
  */
-export const GetLocationList = (PdFlag,pageIndex,pageSize,sortField,sortOrder) => {
+export const getLocationList = (data) => {
+	console.log("getLocationList入参：" + JSON.stringify(data));
 	return uni.request({
 		url: domian + 'common/wms_Server_Data.asp?action=GetLocationList',
-			type: "POST",
+		type: "POST",
 		dataType: "JSON",
-		data: {
-			"PdFlag": 1,
-			"pageIndex": 0,
-			"pageSize":5,
-			"sortField": LocalName,
-			"sortOrder": asc,
-		},
+		data: data
+		// 		data: {
+		// 			"PdFlag": 1,
+		// 			"pageIndex": 0,
+		// 			"pageSize": 5,
+		// 			"sortField": LocalName,
+		// 			"sortOrder": asc,
+		// 		},
 	});
 }
--- pages/deliveryNote/index.vue
+++ pages/deliveryNote/index.vue
@@ -1,347 +1,203 @@
 <template>
 	<view class="content">
 		<view class="example">
-			<view v-if="isShowData" class="wxc-card">
-				<!-- 	<view class="wxc-card_list" v-for="item in data" v-bind:key="item.LocalID"> -->
-				<view class="wxc-card_list_header">
-					<view class="wxc-card_list_header_title">库位ID号:{{ deliveryNote.LocalID }}</view>
-					<view class="wxc-card_list_header_extra">库位名称:{{ deliveryNote.LocalName }}</view>
+			<button type="primary" v-bind:disabled="currentSteps > 1" @click="scanWarehouse">扫库位码</button>
+			<view v-if="LocalID != ''">
+				<view class="wxc-list">
+					<view class="wxc-list-title">库位ID号:</view>
+					<view class="wxc-list-extra">{{ LocalID }}</view>
 				</view>
-				<view class="wxc-card_list_content">
-					<view class="wxc-card_list_content_text">物料编码:{{ deliveryNote.MNumber }}</view>
-					<view class="wxc-card_list_content_text">物料名称:{{ deliveryNote.MName }}</view>
-					<view class="wxc-card_list_content_text">入库时间:{{ deliveryNote.InDate }}</view>
-					<view class="wxc-card_list_content_text">物料供应商:{{ deliveryNote.SupplyName }}</view>
-					<view class="uni-form-item uni-column">
-						<view class="title">输入数量{{EndQuan}}</view>
-						<input class="uni-input" @input="onKeyInput" focus placeholder="请重新输入数量" type="number" step="0.0000000001"
-						 v-enter-number />
-					</view>
+				<view class="wxc-list">
+					<view class="wxc-list-title">库位名称:</view>
+					<view class="wxc-list-extra">{{ LocalName }}</view>
 				</view>
-				<view>
+				<view class="wxc-list">
+					<view class="wxc-list-title">物料编码:</view>
+					<view class="wxc-list-extra">{{ MNumber }}</view>
+				</view>
+				<view class="wxc-list">
+					<view class="wxc-list-title">物料名称:</view>
+					<view class="wxc-list-extra">{{ MName }}</view>
+				</view>
+				<view class="wxc-list">
+					<view class="wxc-list-title">物料供应商:</view>
+					<view class="wxc-list-extra">{{ SupplyName }}</view>
+				</view>
+				<view class="wxc-list">
+					<view class="wxc-list-title">入库时间:</view>
+					<view class="wxc-list-extra">{{ InDate }}</view>
+				</view>
+				<view class="uni-form-item uni-column">
+					<view class="title">输入数量盘点数量</view>
+					<input class="uni-input" v-model="updateEndQuan" focus placeholder="请输需要盘点的数量" type="number" />
 				</view>
 			</view>
-			<button type="primary" @click="Sweeplocation" v-bind:disabled="!Sweeplocations">
-				扫入库码
-			</button>
-			<button type="primary" @click="sureIndelivery" v-bind:disabled="!sureInlibrarys">
-				确认退换
-			</button>
+			<button type="primary" @click="updateAmount" v-bind:disabled="!sureUpdateAmount">确认盘点库存</button>
+			<button type="primary" v-show="currentSteps == 2" @click="goBack">返回</button>
 		</view>
 	</view>
 </template>
+
 <script>
-	// {LocalID:'1',LocalName:'B1-A1-1层-01',MNumber:'1001030001-B12',MName:'后悬置总成',SupplyName:'上海众力汽车零部件有限公司',InDate:'2019/1/26 18:08:22',EndQuan:'82'}
-	import {
-		uniSteps,
-		uniCard,
-		uniList,
-		uniListItem
-	} from '@dcloudio/uni-ui';
-	import {
-		parseWarehouseCode
-	} from '@/libs/util.js';
-	import neilModal from '@/components/neil-modal/neil-modal.vue';
-	import {
-		parseForRule
-	} from '@/libs/util.js';
-	import {
-		SaveInventoryInfo,
-		GetLocalMateInfo
-	} from '@/api/deliveryNote.js';
-	import
-	deliveryNoteModels
-	from '@/model/deliverynoteModel.js'
-	export default {
-		data() {
-			return {
-				data: {
-					LocalID: '',
-					LocalName: '',
-					MNumber: '',
-					MName: '',
-					SupplyName: '',
-					InDate: '',
-					EndQuan: ''
-				},
-				steps: [],
-				currentSteps: 0, //当前执行步骤，
-				currentIndex: 0, //当前需要修改数量的货物索引
-				index: 0,
-				deliveryNote: deliveryNoteModels,
-				isShowData: false,
-				EndQuan: "0",
-				LocalID: '',
-				PdQuan: '',
-				MNumber: '',
-				inputNumber: 12
+import neilModal from '@/components/neil-modal/neil-modal.vue';
+import { authAccount, parseForRule, parseWarehouseCode } from '@/libs/util.js';
+import { getLocalMateInfo, saveInventoryInfo } from '@/api/deliveryNote.js';
+import { mapState } from 'vuex';
+export default {
+	data() {
+		return {
+			currentSteps: 0,
+			LocalID: '', //库位ID号
+			LocalName: '', //库位名称,
+			MNumber: '', //当前库位物料编码
+			MName: '', //当前库位物料名称
+			SupplyName: '', //当前物料供应商
+			InDate: '', //入库时间
+			EndQuan: '', //当前库位数量
+			updateEndQuan: '0'
+		};
+	},
+	computed: {
+		...mapState(['forcedLogin', 'hasLogin', 'userName']),
+		sureUpdateAmount() {
+			if (this.currentSteps == 1) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	},
+	methods: {
+		//设置货架物料信息
+		setStorageInfo: function(data) {
+			try {
+				this.LocalID = data.LocalID;
+				this.LocalName = data.LocalName;
+				this.MNumber = data.MNumber;
+				this.MName = data.MName;
+				this.LocalName = data.LocalName;
+				this.SupplyName = data.SupplyName;
+				this.InDate = data.InDate;
+				this.EndQuan = data.EndQuan;
+				return true;
+			} catch (e) {
+				return false;
 			}
 		},
-		components: {
-			neilModal,
-			uniSteps,
-			uniCard,
-			uniList,
-			uniListItem
-		},
-		computed: {
-			Sweeplocations() {
-				console.log('isCanInlibrary' + this.currentSteps)
-				if (this.currentSteps == 0) {
-					return true;
-				} else {
-					return false;
-				}
-			},
-			sureInlibrarys() {
-				console.log('isCanInlibrary' + this.currentSteps)
-				if (this.currentSteps == 1) {
-					return true;
-				} else {
-					return false;
-				}
-			},
-		},
-		// 			"LocalID": "1",
-		// 		"LocalName": "B1-A1-1层-01",
-		// 		"MNumber": "1001030001-B12",
-		// 		"MName": "后悬置总成",
-		// 		"SupplyName": "上海众力汽车零部件有限公司",
-		// 		"InDate": "2019/1/26 18:08:22",
-		// 		"EndQuan": "82"
-		methods: {
-			onKeyInput: function(event) {
-				this.EndQuan = event.target.value
-			},
-			Sweeplocation: function() {
-				var _this = this;
-				console.log('this定义：' + _this);
-				uni.scanCode({
-					onlyFromCamera: true,
-					success: function(res) {
-						console.log("res.result" + JSON.stringify(res))
-						console.log("res.result" + JSON.stringify(res.result))
-						GetLocalMateInfo(this.LocalID).then(data => {
+		//扫描库位码
+		scanWarehouse: function() {
+			var _this = this;
+			uni.scanCode({
+				onlyFromCamera: true,
+				success: function(res) {
+					console.log('res' + JSON.stringify(res));
+					var result = parseWarehouseCode(res.result);
+					console.log('result' + JSON.stringify(result));
+					if (result && result.codeid && result.codeid != '') {
+						getLocalMateInfo(result.codeid).then(data => {
 							var [error, res] = data;
-							var result = parseForRule(res.data);
-							if (result) {
-								_this.deliveryNote.check(result);
+							console.log('getLocalMateInfo.data:' + JSON.stringify(data));
+							console.log('getLocalMateInfo.res:' + JSON.stringify(res));
+							var checkResult = parseForRule(res.data);
+							console.log('checkResult:' + JSON.stringify(checkResult));
+							if (checkResult && checkResult != {}) {
+								if (_this.setStorageInfo(checkResult)) {
+									_this.currentSteps = 1;
+								} else {
+									uni.showToast({
+										icon: 'none',
+										duration: 2500,
+										title: '物料信息错误：' + JSON.stringify(checkResult)
+									});
+								}
+							} else {
+								uni.showModal({
+									title: '提示',
+									content: '没有获取库位中相关物料信息',
+									showCancel: false,
+									success: function(res) {
+										if (res.confirm) {
+											console.log('用户点击确定');
+										}
+									}
+								});
 							}
-							_this.isShowData = true;
-							_this.currentSteps = 1;
-						})
-					}
-				});
-			},
-			//确认入库
-			sureIndelivery: function() {
-				debugger;
-				console.log(this.EndQuan)
-				console.log("LocalID:" + this.LocalID)
-				SaveInventoryInfo(this.LocalID, this.MNumber, this.PdQuan).then(data => {
-					var [error, res] = data;
-					console.log("data:" + JSON.stringify(data));
-					console.log("res:" + JSON.stringify(res));
-					var result = parseForRule(res.data);
-					var _this = this;
-					console.log(result);
-					if (result.success) {
-						console.log(result);
-						_this.currentSteps = 1;
-						uni.showToast({
-							icon: 'success',
-							title: result.ResponseText
 						});
-						_this.isShowData = false;
 					} else {
-						_this.currentSteps = 0;
 						uni.showToast({
-							icon: 'fail',
-							title: result.ResponseText
+							icon: 'none',
+							duration: 2500,
+							title: '库位信息错误:' + res.result
 						});
-						_this.isShowData = false;
 					}
-				});
-			},
+				}
+			});
 		},
+		//修改盘点数量
+		updateAmount: function() {
+			var _this = this;
+			console.log('updateEndQuan:' + this.updateEndQuan);
+			saveInventoryInfo({"MNumber":this.MNumber,"PdQuan":this.updateEndQuan,"LocalID":this.LocalID}).then(data => {
+				var [error, res] = data;
+				console.log('saveInventoryInfo.data:' + JSON.stringify(data));
+				console.log('saveInventoryInfo.res:' + JSON.stringify(res));
+				var result = parseForRule(res.data);
+				console.log('result:' + JSON.stringify(result));
+				if (result.success) {
+					console.log(result);
+					_this.currentSteps = 2;
+					uni.showToast({
+						icon: 'success',
+						title: '盘点成功！'
+					});
+				} else {
+					uni.showModal({
+						title: '提示',
+						content: result.ResponseText,
+						showCancel: false,
+						success: function(res) {
+							if (res.confirm) {
+								console.log('用户点击确定');
+							}
+						}
+					});
+				}
+			});
+		},
+		//返回
+		goBack: function() {
+			uni.navigateBack();
+		},
+	},
+	onLoad() {
+		authAccount(this.hasLogin, this.forcedLogin, this.userName);
 	}
+};
 </script>
 
 <style lang="scss">
-	.materialnumber {
-		width: auto;
-		height: 100%;
-		float: right;
+button {
+	margin-top: 10px;
+}
+.wxc-list {
+	position: relative;
+	display: flex;
+	flex-direction: row;
+	padding-left: $uni-spacing-col-base;
+	padding-right: $uni-spacing-col-base;
+	padding-right: 0px;
+	align-items: center;
+
+	&-title {
+		flex: 0 0 auto;
+		width: 40%;
+		margin-right: $uni-spacing-col-base;
+		justify-content: flex-start;
+		align-items: right;
 	}
 
-	.bank {
-		width: auto;
-		height: 100%;
-		margin: 0 60%;
-		position: absolute;
+	&-extra {
+		width: 60%;
 	}
-
-	$card-extra-width: 30%;
-
-	@mixin text-omit {
-		text-overflow: ellipsis;
-		white-space: nowrap;
-		overflow: hidden;
-	}
-
-	.uni-card {
-		margin: $uni-spacing-col-base;
-		background: $uni-bg-color;
-		position: relative;
-		display: flex;
-		flex-direction: column;
-
-		&:after {
-			content: '';
-			position: absolute;
-			transform-origin: center;
-			box-sizing: border-box;
-			pointer-events: none;
-			top: -50%;
-			left: -50%;
-			right: -50%;
-			bottom: -50%;
-			border: 1px solid $uni-border-color;
-			border-radius: $uni-border-radius-lg;
-			transform: scale(0.5);
-		}
-
-		&__footer,
-		&__header {
-			position: relative;
-			display: flex;
-			flex-direction: row;
-			padding: $uni-spacing-col-base;
-			align-items: center;
-		}
-
-		&__header {
-			&:after {
-				position: absolute;
-				bottom: 0;
-				right: 0;
-				left: 0;
-				height: 1px;
-				content: '';
-				-webkit-transform: scaleY(0.5);
-				transform: scaleY(0.5);
-				background-color: $uni-border-color;
-			}
-
-			&-title {
-				flex: 1;
-				margin-right: $uni-spacing-col-base;
-				display: flex;
-				flex-direction: row;
-				justify-content: flex-start;
-				align-items: center;
-
-				&-text {
-					font-size: $uni-font-size-lg;
-					flex: 1;
-					@include text-omit;
-				}
-			}
-
-			&-extra {
-				&-img-view {
-					display: flex;
-				}
-
-				&-img {
-					height: $uni-img-size-sm;
-					width: $uni-img-size-sm;
-					margin-right: $uni-spacing-col-base;
-				}
-
-				&-text {
-					flex: 0 0 auto;
-					width: $card-extra-width;
-					margin-left: $uni-spacing-col-base;
-					font-size: $uni-font-size-base;
-					text-align: right;
-					@include text-omit;
-				}
-			}
-
-			&__content {
-				&--pd {
-					padding: $uni-spacing-col-base;
-				}
-			}
-
-			&--pd {
-				padding: $uni-spacing-col-base;
-			}
-		}
-
-		&__footer {
-			justify-content: space-between;
-			color: $uni-text-color-grey;
-			font-size: $uni-font-size-sm;
-			padding-top: 0;
-		}
-	}
-
-	.wxc-list {
-		position: relative;
-		display: flex;
-		flex-direction: row;
-		padding: $uni-spacing-col-base;
-		padding-right: 0px;
-		align-items: center;
-
-		&:after {
-			position: absolute;
-			bottom: 0;
-			right: 0;
-			left: 0;
-			height: 1px;
-			content: '';
-			-webkit-transform: scaleY(0.5);
-			transform: scaleY(0.5);
-			background-color: $uni-border-color;
-		}
-
-		&-title {
-			flex: 1;
-			margin-right: $uni-spacing-col-base;
-			display: flex;
-			flex-direction: row;
-			justify-content: flex-start;
-			align-items: center;
-
-			&-text {
-				font-size: $uni-font-size-base;
-				flex: 1;
-				@include text-omit;
-			}
-		}
-
-		&-extra {
-			&-img-view {
-				display: flex;
-			}
-
-			&-img {
-				height: $uni-img-size-sm;
-				width: $uni-img-size-sm;
-				margin-right: $uni-spacing-col-base;
-			}
-
-			&-text {
-				flex: 0 0 auto;
-				width: $card-extra-width;
-				margin-left: $uni-spacing-col-base;
-				font-size: $uni-font-size-base;
-				text-align: right;
-				@include text-omit;
-			}
-		}
-	}
+}
 </style>
