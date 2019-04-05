43b1f36f2056149a5eaeb5fad837f8efbe04b046
autoyu <yucihai@live.com>
2019-04-05 11:04:57 +0800

    查询接口调试完成

--- api/dismounting.js
+++ api/dismounting.js
@@ -4,37 +4,48 @@ const {
 } = config
 
 /**
-19、拆装件出库
-作用： 用于主机厂不良品到仓储中心调换良品的过程，对良品进行的换货出库的操作
+ * @description 扫描入库前，检查库位是否满足入库要求
+ * 用于物料在移入良品区之前检查库位是否可用，一般在提交数据到数据库保存前检查，
+ * 检查通过则提交数据保存并更新库存。库位存放要求：良品区库位只能存放一种物料，
+ * 冻结库位不能存放物料，不同批次的物料不能存放在同一库位。
  */
-export const SaveAssemOutInfo = (MNumber,Quan,LocalID,Reason) => {
+export const checkLocal = (number, localID) => {
+	console.log("checkLocal:入参 number:" + number + "  localID:" + localID);
 	return uni.request({
-		url: domian + 'common/wms_Server_Data.asp?action=SaveAssemOutInfo',
-			type: "POST",
+		url: domian + 'common/wms_Server_Data.asp?action=CheckLocal',
+		type: "POST",
 		dataType: "JSON",
 		data: {
-			"MNumber": MNumber,
-			"Quan": Quan,
-			"LocalID":LocalID,
-			 "Reason": Reason
+			"MNumber": number,
+			"LocalID": localID
 		},
 	});
 }
-/**
-20、拆装件入库
-作用： 用于主机厂不良品到仓储中心调换良品的过程，对不良品进行的换货入库的操作 
-*/
 
-export const SaveAssemInInfo = (MNumber,Quan,LocalID,Reason) => {
+/**
+ * @description、拆装件入库 组装入库
+ *  用于主机厂不良品到仓储中心调换良品的过程，对不良品进行的换货入库的操作 
+ */
+export const saveAssemInInfo = (data) => {
+	console.log("saveAssemInInfo:入参 :"+ JSON.stringify(data));
 	return uni.request({
 		url: domian + 'common/wms_Server_Data.asp?action=SaveAssemInInfo',
-			type: "POST",
+		type: "GET",
 		dataType: "JSON",
-		data: {
-			"MNumber": MNumber,
-			"Quan": Quan,
-			"LocalID":LocalID,
-			 "Reason": Reason
-		},
+		data: data
+	});
+}
+
+/**
+ * @description拆装件出库 组装出库
+ * 用于主机厂不良品到仓储中心调换良品的过程，对良品进行的换货出库的操作
+ */
+export const saveAssemOutInfo = (data) => {
+	console.log("saveAssemOutInfo:入参 :"+ JSON.stringify(data));
+	return uni.request({
+		url: domian + 'common/wms_Server_Data.asp?action=SaveAssemOutInfo',
+		type: "GET",
+		dataType: "JSON",
+		data: data
 	});
 }
